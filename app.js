029f332c6b3f5a4cbf24df1aba490c256b255e11
Ashraful-Alam-FrontDev <152710448+Ashraful-Alam-FrontDev@users.noreply.github.com>
2023-12-06 21:28:17 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', () =>{
+    const faqContainer = document.querySelector('.faq-accordion');
+
+    faqContainer.addEventListener('click', (e)=>{
+           const gruopHeader = e.target.closest('.faq-dropdown-header');
+
+           if(!gruopHeader) return;
+
+           const group = gruopHeader.parentElement;
+           const groupbody = group.querySelector('.faq-dropdown-body');
+           const icon = gruopHeader.querySelector('.faq-icon');
+
+           icon.classList.toggle('open');
+           groupbody.classList.toggle('open');
+
+           const otherGroups = faqContainer.querySelectorAll('.faq-dropdown');
+
+           otherGroups.forEach((otherGroup)=>{
+            if(otherGroup!== group){
+                const otherGroupBody = otherGroup.querySelector('.faq-dropdown-body');
+                const otherIcon = otherGroup.querySelector('.faq-icon');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('open');
+            }
+
+           });
+
+ });
+ });
